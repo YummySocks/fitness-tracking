a96ad7a36269e8515bb55f20264b857e3918d124
Socks <patricksutcliffe123@gmail.com>
2021-11-16 19:00:07 -0500

    added routes for posted and getting workouts

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const path = require('path');
+const db = require("../models");
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
@@ -12,5 +13,29 @@ router.get('/exercise', (req, res) => {
 router.get('/stats', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/stats.html'));
 });
+
+router.get("/api/workouts", (req, res) =>
+  //   db.Workout.find({})
+  db.Workout.aggregate([
+    // { match: { _id: req.params.id } },
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+        totalDistance: { $sum: "$exercises.distance" },
+        totalSets: { $sum: "$exercises.sets" },
+        totalReps: { $sum: "$exercises.reps" },
+        totalWeight: { $sum: "$exercises.weight" },
+      },
+    },
+  ])
+    .then((workout) => res.json(workout))
+    .catch((err) => res.status(400).json(err))
+);
+
+router.post("/api/workouts", (req, res) =>
+  db.Workout.create({})
+    .then((workout) => res.json(workout))
+    .catch((err) => res.status(400).json(err))
+);
 
 module.exports = router;
